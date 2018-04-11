58887ea1d994c800172ea75877e112a4c66b8fa3
iZeaoGamer <jameshayward14@icloud.com>
2018-04-11 13:14:16 +0100

    Update ChestShop.php

--- ChestShopNewAPI/src/ChestShop/ChestShop.php
+++ ChestShopNewAPI/src/ChestShop/ChestShop.php
@@ -5,39 +5,18 @@ namespace ChestShop;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
 
-//MassiveEconomy API Call
-use onebone\economyapi\EconomyAPI;
-
 class ChestShop extends PluginBase
 {
-
 	public function onLoad()
 	{
 	}
-
 	public function onEnable()
 	{
 		if (!file_exists($this->getDataFolder())) @mkdir($this->getDataFolder());
 			$this->getServer()->getPluginManager()->registerEvents(new EventListener($this, new DatabaseManager($this->getDataFolder() . 'ChestShop.sqlite3')), $this);
 			$this->getLogger()->info(TextFormat::GREEN . "Enabled");
-		}else{
-			$this->getLogger()->alert(TextFormat::RED . "Plugin disabled.");
-			$this->getPluginLoader()->disablePlugin($this);
-		}
-
 	}
-
 	public function onDisable()
 	{		
 	}
 }
-
-
-
-
-
-
-
-
-
-
